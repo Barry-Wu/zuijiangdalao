0b88824177cb3bec94aba55340572e70c809a672
Barry-Wu <56628027+Barry-Wu@users.noreply.github.com>
2019-10-16 23:41:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,5 +10,5 @@ import tensorflow as tf
 import tensorflow.contrib.rnn as rnn
 import tensorflow.contrib.legacy_seq2seq as seq2seq
 
-python poetry_gen.py --mode train
-python poetry_gen.py --mode sample
+python gen.py --mode train
+python gen.py --mode sample
