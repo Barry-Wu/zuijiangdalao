7dea78c35175b9b953ba49bf35c6fc59aaee0cbe
Barry-Wu <56628027+Barry-Wu@users.noreply.github.com>
2020-07-14 18:13:46 +0800

    Update C练习生.c

--- 程序练习生/C练习生.c
+++ 程序练习生/C练习生.c
@@ -6,15 +6,18 @@
 请编写输出以下图案的程序，图案的行数由输入的整数值确定（每行中字符之间没有空格）。
 整活如下：
 */
-void lettertower(int x)                         /*字母金字塔函数，参数就是层数*/
-{                                               /*如果层数是7，就会打出：*/
-	for(int i=0;i<x;i++)                          /      A      /
-    for(int j=0;j<=2*x-2;j++)                   /     BBB     /
-      {                                         /    CCCCC    /
-        if(x-1-i<=j&&j<=x-1+i)putchar('A'+i);   /   DDDDDDD   /
-        else putchar('')                        /  EEEEEEEEE  /
-      }                                         / FFFFFFFFFFF /
-}                                               /GGGGGGGGGGGGG/
+void lettertower(int x)								/*字母金字塔函数，参数就是层数*/
+{													/*如果层数是7，就会打出：*/
+	for(int i=0;i<x;i++)							/*      A      */
+	{												/*     BBB     */
+		for(int j=0;j<=2*x-2;j++)					/*    CCCCC    */
+		{											/*   DDDDDDD   */
+			if(x-1-i<=j&&j<=x-1+i)putchar('A'+i);   /*  EEEEEEEEE  */
+			else putchar(' ');						/* FFFFFFFFFFF */
+		}											/*GGGGGGGGGGGGG*/
+		putchar('\n');
+	}
+}
 /*一定要刻在DNA里的主函数*/
 int main()
 {
